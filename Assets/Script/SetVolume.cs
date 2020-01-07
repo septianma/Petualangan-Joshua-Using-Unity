27f4cb7c4ca2a265494c817619d9aad426cfde2e
septianmuhammadadi <septianmuhammadadi@gmail.com>
2020-01-08 01:44:14 +0700

    Setting Set Volume Backsound

---
+++ Assets/Script/SetVolume.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class SetVolume : MonoBehaviour {
+
+    public AudioMixer mixer;
+	public Slider slider;
+
+	void Start(){
+		slider.value = PlayerPrefs.GetFloat("Music", 0f);
+	}
+
+    public void SetLevel (float sliderValue)
+    {
+        mixer.SetFloat("volBacksound", Mathf.Log10(sliderValue) * 20);
+		PlayerPrefs.SetFloat("Music", sliderValue);
+    }
+}
