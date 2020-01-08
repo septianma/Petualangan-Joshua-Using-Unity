46a201cdbcd654cb90d5ccc14372aeb67c9ecbe8
septianmuhammadadi <septianmuhammadadi@gmail.com>
2020-01-08 20:35:30 +0700

    Fix Bug

---
+++ Assets/Script/GameEND.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameEND : MonoBehaviour {
+
+	public int nextSceneLoad;
+
+	void Start () {
+		nextSceneLoad = SceneManager.GetActiveScene().buildIndex - 7;
+	}
+
+	public void LoadNextLevel(){
+		SceneManager.LoadScene(nextSceneLoad);
+		if(nextSceneLoad > PlayerPrefs.GetInt("levelAt")){
+			PlayerPrefs.SetInt("levelAt", nextSceneLoad);
+		}
+	}
+}
